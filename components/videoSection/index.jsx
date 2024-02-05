c4467afa50baf8968f843f3db6c231d87f4531aa
julia.delis <julia.delis@idwall.co>
2024-02-05 10:07:34 -0300

    chore: adding video section

--- components/videoSection/index.jsx
+++ components/videoSection/index.jsx
@@ -1,8 +1,60 @@
+"use client";
+
+import { useRef, useEffect } from "react";
 import React from "react";
 import styles from "./style.module.scss";
 
 const VideoSection = () => {
-  return <section>videoSection</section>;
+  const container = useRef(null);
+  const stickyMask = useRef(null);
+  const initialMaskSize = 0.5;
+  const targetMaskSize = 20;
+  const easing = 0.25;
+  let easedScrollProgress = 0;
+
+  useEffect(() => {
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    const maskSizeProgress = targetMaskSize * getScrollProgress();
+    stickyMask.current.style.webkitMaskSize =
+      (initialMaskSize + maskSizeProgress) * 100 + "%";
+
+    console.log(maskSizeProgress);
+    requestAnimationFrame(animate);
+  };
+
+  const getScrollProgress = () => {
+    const scrollProgress =
+      stickyMask.current.getBoundingClientRect().top /
+      (window.innerHeight - container.current.getBoundingClientRect().height);
+    const delta = scrollProgress - easedScrollProgress;
+
+    console.log({
+      boudH: container.current.getBoundingClientRect().height,
+      windowInnerHeight: window.innerHeight,
+      bottom:
+        container.current.getBoundingClientRect().height - window.innerHeight,
+      refSticky: stickyMask.current,
+      sticky: stickyMask.current.offsetTop,
+    });
+    console.log({ easing });
+    easedScrollProgress += delta * easing;
+    return easedScrollProgress;
+  };
+
+  return (
+    <section className={styles.section}>
+      <div ref={container} className={styles.container}>
+        <div ref={stickyMask} className={styles.stickyMask}>
+          <video autoPlay muted loop>
+            <source src="/images/era.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default VideoSection;
