d829db97526a16d96ac4ea1b7fb78802abeb3da9
Julia Delis <juliadelis2@hotmail.com>
2024-04-30 09:16:13 -0300

    feat: controls to mobile video

--- components/videomobile/index.jsx
+++ components/videomobile/index.jsx
@@ -6,7 +6,7 @@ const VideoMobile = () => {
   return (
     <section id="videoSection" className={styles.section}>
       <div className={styles.container}>
-        <video id="video" muted autoPlay loop>
+        <video id="video" muted controls autoPlay loop>
           <source src="/images/era.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
